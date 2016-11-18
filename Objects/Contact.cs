3732307cfcda0a6131924006da10748da60b3694
Loren Glenn <lorencglenn@gmail.com>
2016-11-18 07:38:27 -0800

    add Contact class with constructor

---
+++ Objects/Contact.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace AddressBook.Objects
+{
+  public class Contact
+  {
+    private string _name;
+    private string _address;
+    private string _phoneNumber;
+    private int _id;
+    private static List<Contact> _contacts = new List<Contact> {};
+
+    public Contact(string name, string address, string phoneNumber)
+    {
+      _name = name;
+      _address = address;
+      _phoneNumber = phoneNumber;
+      _id = _contacts.Count;
+      _contacts.Add(this);
+      
+    }
+  }
+}
